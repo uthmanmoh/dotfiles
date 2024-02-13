a6bc4009d423c564cc2a4e53beba695ffaee4129
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2024-02-13 15:13:18 -0500

    feat(tmux): improve tmux status line style and directory name

--- scripts/shorten_path.sh
+++ scripts/shorten_path.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# Read from stdin
+# Read current path from stdin
 input=$(cat)
 
-# Replace $HOME with ~
-modifiedPath=${input/#$HOME/\~}
+# Attempt to get the root directory of the Git repo (if exists)
+git_root=$(git -C "$input" rev-parse --show-toplevel 2>/dev/null)
 
-# Process the path with awk
-echo "$modifiedPath" | awk -F '/' '{if(NF > 4){print "/…/"$(NF-2)"/"$(NF-1)"/"$(NF)}else{print}}'
+if [ -n "$git_root" ]; then
+	# If inside a Git repo, extract and display the name of the root folder
+	echo "$(basename "$git_root")"
+else
+	# If not in a Git repo, proceed with the original path shortening logic
+
+	# Replace $HOME with ~
+	modifiedPath=${input/#$HOME/\~}
+
+	# Process the path with awk
+	echo "$modifiedPath" | awk -F '/' '{if(NF > 3){print "/…/"$(NF-1)"/"$(NF)}else{print}}'
+fi

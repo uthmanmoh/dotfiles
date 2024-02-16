980f1283c4e9ccf384062910016e35baf0233a82
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2024-02-16 17:41:28 -0500

    docs(zsh): add comments to zsh aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,24 +1,18 @@
-#### SCHOOL ALIASES
-alias school="cd ~/Documents/SchoolWork/Year3Sem2"
-
-alias mars="java -jar ~/Documents/SchoolWork/Year2Sem2/CSCB58/Mars4_5.jar"
-
-alias start-vm='VBoxManage startvm "vm"'
-
 ### GENERAL
 alias python="python3"
 alias python2="~/.pyenv/versions/2.7.18/bin/python"
-alias ls="ls -GF"
-alias ll="ls -lArth"
+alias ls="ls -GF" # enable colours and file type indicators
+alias ll="ls -lArth" # ls with long format sorted by modified date
 alias lss="ls -lArSh" # ls sort by size
-alias grep="grep --color=auto -i"
+alias grep="grep --color=auto -i" # case insensitive grep with colour
+
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias tree="eza --tree --level=3 --icons --git --git-ignore -A --sort=modified"
 alias vi="nvim"
-
 alias jq="jaq"
+
 alias sz="source ~/.zshrc"
 alias r="omz reload"
 
@@ -26,12 +20,14 @@ alias battery="system_profiler SPPowerDataType | grep Cycle"
 
 alias ai="gh copilot suggest -t shell"
 
+# alias cd to use zoxide github.com/ajeetdsouza/zoxide
 eval "$(zoxide init --cmd cd zsh)"
 
 export NEOVIM_CONFIG=~/.config/nvim
 
-bindkey -M viins '^L' vi-forward-char # ctrl + l to replace right arrow key (useful in vi mode)
+bindkey -M viins '^L' vi-forward-char # ctrl + l to replace right arrow key (to accept autocomplete)
 
+# Use fzf to find files from ~ and cd into the directory of the selected file
 f() {
     local selection
     export FZF_DEFAULT_COMMAND="fd --type f --type d"
@@ -45,21 +41,6 @@ f() {
             cd "$(dirname "$selection")"
         fi
     fi
-}
-
-len() {
-    local concatenated_string="$*"
-    echo ${#concatenated_string}
-}
-
-term() {
-  # if $1 passed in, open -a "iTerm" in that directory
-  # else open -a "iTerm" in current directory
-  if [ -z "$1" ]; then
-    open -a "iTerm" .
-  else
-    open -a "iTerm" "$1"
-  fi
 }
 
 # Create a new obsidian "vault" by copying a base obsidian config
--- .zsh/bat.zsh
+++ .zsh/bat.zsh
@@ -1,7 +1,6 @@
-# bat
-export MANPAGER="sh -c 'col -bx | bat -l man -p'"
-alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
-alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+export MANPAGER="sh -c 'col -bx | bat -l man -p'" # Use bat as manpager
+alias -g -- -h='-h 2>&1 | bat --language=help --style=plain' # Use bat for -h help
+alias -g -- --help='--help 2>&1 | bat --language=help --style=plain' # ... or --help
 
-alias cat='bat --style=plain'
+alias cat='bat --style=plain' # Use bat as cat
 

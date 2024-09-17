df23d1ed815b63c479d3b1c3ceecf020b3b7259d
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2024-09-17 19:35:52 -0400

    feat(setup.sh): add installs for fonts, git, delta, omz, p10k, tpm, nvm

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ function recho {
 
 # If program is not installed using brew, run brew install on it
 function install_brew {
-  (brew list | grep -q $1 >/dev/null && gecho "$1 found...") ||
+  (brew list | grep -q "^$1$" >/dev/null && gecho "$1 found...") ||
     (yecho "$1 not found, installing via homebrew..." && brew install $1)
 }
 
@@ -54,24 +54,59 @@ function linkdotfile {
 }
 
 # Make sure homebrew is installed
-[[ ! $(command -v brew >/dev/null) ]] ||
-  recho "Homebrew not found, install before proceeding."
+command -v brew >/dev/null || recho "Homebrew not found, install before proceeding."
 
-# install Homebrew main programs
-BREW_PROGRAMS=("bat" "fd" "fzf" "gcc" "git" "lazygit" "neovim" "pngpaste" "ripgrep" "tmux" "zoxide")
+# Make sure zsh is installed
+command -v zsh >/dev/null || recho "zsh not found, install before proceeding."
 
-for program in ${BREW_PROGRAMS[@]}; do
-  install_brew $program
+# Make sure curl is installed
+command -v curl >/dev/null || recho "curl not found, install before proceeding."
+
+FONT="font-jetbrains-mono-nerd-font"
+install_brew $FONT
+
+# Install Homebrew main programs
+BREW_PROGRAMS="bat fd fzf gcc git git-delta lazygit neovim pngpaste ripgrep tmux zoxide"
+
+for program in $BREW_PROGRAMS; do
+  install_brew "$program"
 done
 
-HOME_FILES=(".fdignore" ".gitconfig" ".ideavimrc" ".p10k.zsh" ".tmux.conf" ".scripts" ".zsh" ".zshrc")
+# Install oh-my-zsh if not installed
+if [[ ! -d ~/.oh-my-zsh ]]; then
+  yecho "oh-my-zsh not found, installing..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
-for file in "${HOME_FILES[@]}"; do
+# Install powerlevel10k
+if [[ ! -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]]; then
+  yecho "powerlevel10k not found, installing..."
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+
+# Install nvm (and node)
+if [[ ! -d ~/.nvm ]]; then
+  yecho "nvm not found, installing..."
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+  unalias nvm node npm yarn && unset nvm_cmds && . ~/.nvm/nvm.sh --no-use && load-nvmrc && nvm
+  nvm install node
+fi
+
+# Install tpm for tmux
+if [[ ! -d ~/.tmux/plugins/tpm ]]; then
+  yecho "tmux plugin manager not found, installing..."
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  tmux source-file ~/.tmux.conf
+fi
+
+HOME_FILES=".fdignore .gitconfig .ideavimrc .p10k.zsh .tmux.conf .scripts .zsh .zshrc"
+
+for file in $HOME_FILES; do
   linkdotfile "$file" "$HOME/$file"
 done
 
 linkdotfile .config/alacritty ~/.config/alacritty
 linkdotfile .config/git ~/.config/git
-linkdotfile .lazygit-config.yml "$HOME/Library/Application Support/lazygit/config.yml"
-linkdotfile .obsidian "$HOME/.config/obsidian"
+linkdotfile .obsidian ~/.config/obsidian
 linkdotfile karabiner.json ~/.config/karabiner/karabiner.json
+linkdotfile .lazygit-config.yml "$HOME/Library/Application Support/lazygit/config.yml"

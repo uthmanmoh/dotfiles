bef8a0f1d5e2f048283cf0351ddd0172617adaf3
Uthman Mohamed <uthmanmoh@icloud.com>
2024-10-04 16:21:40 -0400

    feat(tmux): add tmux 3.4 installation script

---
+++ .scripts/install-tmux-3-4.sh
@@ -0,0 +1,4 @@
+brew uninstall tmux
+brew unlink tmux
+wget https://raw.githubusercontent.com/Homebrew/homebrew-core/62acbfc97e3148200e86eb38bfc998145c8e1830/Formula/t/tmux.rb
+brew install ./tmux.rb

9725dd102afb9012e75c68f469775389b72a67f7
Uthman Mohamed <uthmanmo@amazon.com>
2025-01-20 15:11:57 +0000

    feat(sesh): add sesh config

--- setup.sh
+++ setup.sh
@@ -131,6 +131,7 @@ done
 linkdotfile .config/alacritty ~/.config/alacritty
 linkdotfile .config/git ~/.config/git
 linkdotfile .config/bat ~/.config/bat
+linkdotfile .config/sesh ~/.config/sesh
 linkdotfile .obsidian ~/.config/obsidian
 linkdotfile karabiner.json ~/.config/karabiner/karabiner.json
 linkdotfile .scripts/shorten_path.sh ~/.tmux/scripts/shorten_path.sh
